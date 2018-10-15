0997ee5b59005c8400f2b2676a33456f69d2168d
Aizar <aizar.cabrera@icloud.com>
2018-10-15 13:29:26 -0600

    adds test for status in board class

--- test/board_test.rb
+++ test/board_test.rb
@@ -28,6 +28,11 @@ class BoardTest < Minitest::Test
     board = Board.new
 
     assert_equal ' ', board.populate_board['A1'].display
+  end
+
+  def test_it_can_have_a_status
+    board = Board.new
+
     assert_equal 0, board.populate_board['A1'].status
   end
 
