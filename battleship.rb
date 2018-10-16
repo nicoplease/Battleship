d2123c11db40faec1188a14d3aba0f8c461b9609
Aizar <aizar.cabrera@icloud.com>
2018-10-16 13:10:13 -0600

    adds runner file

---
+++ battleship.rb
@@ -0,0 +1,37 @@
+require './lib/game'
+require './lib/player'
+require 'pry'
+
+class Battleship
+
+  def initialize
+    start
+  end
+
+  def start
+    puts "Welcome to BATTLESHIP"
+    puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+    print ">"
+    input = gets.chomp
+    if input == 'p' || input =='play'
+      play
+      elsif input == 'i' || input =='instructions'
+        puts 'Ships cannot wrap around the board'
+        puts 'Ships cannot overlap'
+        puts 'Ships can be laid either horizontally or vertically'
+        puts 'Coordinates must correspond to the first and last units of the ship.'
+        puts  '(IE: You can’t place a two unit ship at “A1 A3”)'
+      elsif input == 'q' || input =='quit'
+        puts "OK,bye...!"
+      else
+        puts 'Invalid answer, please, try again'
+        start
+    end
+  end
+
+  def play
+    Game.begin
+  end
+
+end
+Battleship.new
