6669af31c8464397b1065a79f7abd80aa2a7d167
Aizar <aizar.cabrera@icloud.com>
2018-10-17 14:53:09 -0600

    adds setup method for game test

--- test/game_test.rb
+++ test/game_test.rb
@@ -2,55 +2,43 @@ require './test/test_helper'
 
 class GameTest < Minitest::Test
 
-  def test_it_exists
-    game = Game.new
+  def setup
+  @game = Game.new
+  end
 
-    assert_instance_of Game, game
+  def test_it_exists
+    assert_instance_of Game, @game
   end
 
   def test_it_can_have_a_board
-    game = Game.new
-
-    assert_instance_of Board, game.board
+    assert_instance_of Board, @game.board
   end
 
   def test_it_can_have_player
-    game = Game.new
-
-    assert_instance_of Player, game.player1
+    assert_instance_of Player, @game.player1
   end
 
   def test_it_can_display_board
-    game = Game.new
-
-    assert_equal [["D"], [" "], [" "], [" "], [" "]], game.board.display
+    assert_equal [["D"], [" "], [" "], [" "], [" "]], @game.board.display
   end
 
   def test_ships_can_have_coordinates
-    game = Game.new
     # ship1 = Ship.new(2)
     # ship2 = Ship.new(3)
-
-    assert_equal %W[A1 A2], game.ship1.record_coordinates("A1 A2")
-    assert_equal %w[B1 B3 B2], game.ship2.record_coordinates("B1 B3") #overlap later !!
-  end
-
-  def test_Gameboard_can_store_ship_coordinates
-    game = Game.new
-    # ship1 = Ship.new(2)
-    # ship2 = Ship.new(3)
-
-    assert_equal %w[A1 A2], game.store_destroyer_spots
+    assert_equal %W[A1 A2], @game.ship1.record_coordinates("A1 A2")
+    assert_equal %w[B1 B3 B2], @game.ship2.record_coordinates("B1 B3") #overlap later !!
   end
 
   def test_gameboard_can_store_ship_coordinates
-    game = Game.new
+    # ship1 = Ship.new(2)
     # ship2 = Ship.new(3)
-
-    assert_equal %w[B1 B3 B2], game.store_submarine_spots
+    assert_equal %w[A1 A2], @game.store_destroyer_spots
   end
 
-  # def test_game_can_place_ship_cells
+  def test_gameboard_can_store_ship_coordinates
+    # ship2 = Ship.new(3)
+    assert_equal %w[B1 B3 B2], @game.store_submarine_spots
+  end
 
 
 end
