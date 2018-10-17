b8339eb8b84d31f4280ff0ea9a0aea54e14e8291
Aizar <aizar.cabrera@icloud.com>
2018-10-17 14:36:41 -0600

    adds test helper for board test

--- test/board_test.rb
+++ test/board_test.rb
@@ -1,7 +1,4 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/board'
-require './lib/cell'
+require './test/test_helper'
 
 class BoardTest < Minitest::Test
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/board'
 require './lib/cell'
 require './lib/ship'
