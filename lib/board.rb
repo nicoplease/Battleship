141f263393e2ef7766fd2e38f1c4e7e3e7b0ca20
Nico <nicovigil1@gmail.com>
2018-10-17 16:36:50 -0600

    adds cell's ability to hold a specific ship instance

--- lib/board.rb
+++ lib/board.rb
@@ -3,23 +3,30 @@ require 'pry'
 
 class Board
 
-  attr_reader :cells
+  attr_reader :cells, :cell_names
 
   def initialize
 
+  @cell_names = {:A1 => Cell.new('A', '1'), :A2 => Cell.new('A', '2'), :A3 => Cell.new('A', '3'), :A4 => Cell.new('A', '4'),
+    :B1 => Cell.new('B', '1'), :B2 => Cell.new('B', '2'), :B3 => Cell.new('B', '3'), :B4 => Cell.new('B', '4'),
+    :C1 => Cell.new('C', '1'), :C2 => Cell.new('C', '2'), :C3 => Cell.new('C', '3'), :C4 => Cell.new('C', '4'),
+    :D1 => Cell.new('D', '1'), :D2 => Cell.new('D', '2'), :D3 => Cell.new('D', '3'), :D4 => Cell.new('D', '4'),
+  }
+
   @cells = [ [["O"],["1"],["2"],["3"],["4"]],
-            [["A"], Cell.new('A', '1').display, Cell.new('A', '2').display, Cell.new('A', '3').display, Cell.new('A', '4').display],
-            [["B"], Cell.new('B', '1').display, Cell.new('B', '2').display, Cell.new('B', '3').display, Cell.new('B', '4').display],
-            [["C"], Cell.new('C', '1').display, Cell.new('C', '2').display, Cell.new('C', '3').display, Cell.new('C', '4').display],
-            [["D"], Cell.new('D', '1').display, Cell.new('D', '2').display, Cell.new('D', '3').display, Cell.new('D', '4').display]]
+            [["A"], @cell_names[:A1].display, @cell_names[:A2].display, @cell_names[:A3].display, @cell_names[:A4].display],
+            [["B"], @cell_names[:B1].display, @cell_names[:B2].display, @cell_names[:B3].display, @cell_names[:B4].display],
+            [["C"], @cell_names[:C1].display, @cell_names[:C2].display, @cell_names[:C3].display, @cell_names[:C4].display],
+            [["D"], @cell_names[:D1].display, @cell_names[:D2].display, @cell_names[:D3].display, @cell_names[:D4].display] ]
+
   end
 
   def display
     p @cells[0]
     p @cells[1]
-    p @cells[2]
-    p @cells[3]
-    p @cells[4]
+    p @cells[2]#[1..-1].display
+    p @cells[3]#[1..-1].display
+    p @cells[4]#[1..-1].display
   end
 
   def populate_board # used to affect those methods
--- lib/game.rb
+++ lib/game.rb
@@ -2,14 +2,17 @@ require 'pry'
 require "./lib/board"
 require './lib/ship'
 
+
 class Game
-attr_reader :board, :player1, :ship1, :ship2
+attr_reader :board, :player1, :ship1, :ship2, :destroyer_spots
 
   def initialize
-    @board   = Board.new
+    @board = Board.new
     @player1 = Player.new
-    @ship1   = Ship.new(2)
-    @ship2   = Ship.new(3)
+    @ship1 = Ship.new(2)
+    @ship2 = Ship.new(3)
+    @ship_spots
+    @destroyer_spots = []
   end
 
   def display_board
@@ -19,21 +22,32 @@ attr_reader :board, :player1, :ship1, :ship2
   def store_destroyer_spots
     p "Place your 2 space Destroyer"
     print ">> "
-    # input = gets.chomp #- for implementation with battleship.rb
-    input = "A1 A2"
-    ship_spots = []
-    ship_spots << @ship1.record_coordinates(input)
-    ship_spots.flatten
+    input = gets.chomp #- for implementation with battleship.rb
+    # input = "A1 A2"
+    @destroyer_spots << @ship1.record_coordinates(input.upcase)
+    @destroyer_spots = @destroyer_spots.flatten
   end
 
   def store_submarine_spots
     p "Place your 3 space Submarine"
     print ">> "
-    # input = gets.chomp #- for implementation with battleship.rb
-    input = "B1 B3"
-    ship_spots = []
-    ship_spots << @ship2.record_coordinates(input)
-    ship_spots.flatten
+    input = gets.chomp #- for implementation with battleship.rb
+    # input = "B1 B3"
+    sub_spots = []
+    sub_spots << @ship2.record_coordinates(input.upcase)
+
+    sub_spots.flatten
+  end
+
+  def place_destroyer_in_cells
+    coordinates = {"A"=>1, "B"=>2, "C"=>3, "D"=>4, "1"=>1, "2"=>2, "3"=>3, "4"=>4}
+    @destroyer_spots.map do |spot|
+      @board.cell_names[spot.to_sym].place_ship
+      @board.cell_names[spot.to_sym].ships << @ship1      
+      @board.cell_names[spot.to_sym]
+    end
   end
 
 end
+
+binding.pry
